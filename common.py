8b72d83f13c235f932c0fe1cf76e5298b5d90733
Pushkar Pathak <pushkarpathak21@gmail.com>
2019-10-25 14:54:44 +0530

    Update common.py

--- common.py
+++ common.py
@@ -6,5 +6,4 @@ CLASS_NAME = [
     'Nike', 'Pepsi', 'Porsche', 'Puma', 'RedBull', 'Sprite', 'Starbucks',
     'Texaco', 'Unicef', 'Vodafone', 'Yahoo'
 ]
-PMAK-5da98cc8032add002a9f910b-317566983394a7058a64ede7571f5fd132
-PMAK-5d9f16561886ba00362108a2-7108b9ded271e6939b5705923d25862111
+
